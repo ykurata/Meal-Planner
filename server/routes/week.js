9d1c54640340106e1047c4fd01a1081f03d48d4e
Yasuko Kurata <yasukokurata@gmail.com>
2021-02-25 16:56:43 -0500

    add and test get week by id and delete week routes

--- server/routes/week.js
+++ server/routes/week.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { rawListeners } = require('../app');
 const router = express.Router();
 
 const Week = require('../models/Week');
@@ -9,7 +10,7 @@ router.post("/", async (req, res) => {
       name: req.body.name,
       days: req.body.days
     });
-    const newWeek = await WeakSet.save();
+    const newWeek = await week.save();
     return res.status(200).json(newWeek);
   } catch (err) {
     console.log(err);
@@ -19,7 +20,32 @@ router.post("/", async (req, res) => {
 router.get("/all", async (req, res) => {
   try {
     const weeks = await Week.find({});
+    return res.status(200).json(weeks);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// Create week first and put(update) days. 
+
+
+// Find a week by week id
+router.get("/get/:id", async (req, res) => {
+  try {
+    const week = await Week.findOne({ _id: req.params.id});
     return res.status(200).json(week);
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+// Delete week
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const week = await Week.findOne({ _id: req.params.id});
+    await week.remove();
+    return res.status(200).json({ "message": "Succeccfully deleted"});
   } catch (err) {
     console.log(err);
   }
